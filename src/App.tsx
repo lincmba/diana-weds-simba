dfc046a926bbc6bfbc34900a806da353007b9a64
lincmba <lincolncmba@gmail.com>
2026-03-20 06:10:38 +0300

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -57,7 +57,7 @@ function App() {
             </div>
 
             <p className="text-base sm:text-lg italic pt-4 font-semibold" style={{ fontFamily: "'Cormorant Garamond', serif", color: '#1a3a52', opacity: 0.85 }}>
-               Your presence will mean the world to us,
+               Your presence will mean the world to us, 
                <br className="hidden sm:block" />
                and any support towards our new beginning will be deeply appreciated.
             </p>
